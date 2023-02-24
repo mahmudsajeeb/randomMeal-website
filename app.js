4771066453b7c2b505d63fb761c233b80e79ab29
Mahmud sajib <mahmudsajib082@gmail.com>
2023-02-25 02:44:39 +0600

    model add

--- app.js
+++ app.js
@@ -26,12 +26,40 @@ const displayMeal = meals =>{
     <div class="card-body">
       <h5 class="card-title">${meal.strMeal}</h5>
       <p class="card-text">${meal.strInstructions}</p>
+
+      <!-- Button trigger modal -->
+<button onclick=mealModaldetail(${meal.idMeal}) type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealdetailsModal">
+    Details
+  </button>
     </div> 
+
+    
   </div>
+  
+      
     `
     mealcontainer.appendChild(mealDiv)
   })
 
+}
+
+/// modal detail 
+
+const mealModaldetail = idMeal =>{
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayModalMealDetal(data.meals[0]))
+}
+
+const displayModalMealDetal = meal =>{
+  document.getElementById('mealdetailsModalLabel').innerText = meal.strMeal;
+  const mealBody = document.getElementById("mealdetailsModalBody")
+  mealBody.innerHTML = `
+    <img class="img-fluid" src=${meal.strMealThumb} />
+    
+    <a href="">Youtube: ${meal.strYoutube}</a>
+  `
 }
  // search meal food item using input buttton 
 
